59c921c20406ff690a1300689c70f647141a43b9
amironich1993@gmail.com <mirochnikag@pervobank.ru>
2016-04-06 15:28:39 +0400

    fromrep

--- SimpleDotComGame.java
+++ SimpleDotComGame.java
@@ -1,24 +1,27 @@
-import java.lang.String;
-import java.lang.System;
+
+
+import java.util.Scanner;
 
 public class SimpleDotComGame {
     public static void main(String[] args)
     {
-        //System.out.print("TEST");
-        int numOfGuess=0;
+        int numOfGuesses = 0;
         GameHelper helper = new GameHelper();
+
         SimpleDotCom theDotCom = new SimpleDotCom();
-        int randomNum = (int)(Math.random()*5);
-        int[] locations = {randomNum,randomNum+1,randomNum+2};
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
         theDotCom.setLocationCells(locations);
-        boolean isAlive=true;
-        while (isAlive!=true){
-            String guess = helper.getUserInput("VVEDITE CHISLO");
+        boolean isAlive = true;
+        while (isAlive == true)
+        {
+            String guess = helper.getUserInput("enter a number");
             String result = theDotCom.checkYourself(guess);
-            numOfGuess++;
-            if (result.equals("POTOPIL")){
-                isAlive=false;
-                System.out.println("VAM NUZHNO BILO "+ numOfGuess + " POPITOK");
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
             }
         }
     }
